76f9e60fd1451fb3363a0d89ec3ec083f7b451f9
sh-mirjalol-3930 <mirjalolshomurodov179@gmail.com>
2025-01-13 21:32:52 +0500

    firs-commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let L = 1000;
+let m = L / 100; 
+console.log(m); 
+
+
+
+
+let M = 15000;
+let kg = M / 1000;
+console.log(kg);
+
+
+
+
+let H = 1000;
+let cm = H / 100;
+console.log(cm);
+
+
+
+let S = 15000;
+let m2 = S / 10000;
+console.log(m2);    
+
+
+
+let T = 15000;
+let s = T / 10000;
+console.log(s);
+
+
+

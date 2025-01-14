dcee2e5361b8bb77223c3ffd2521db397915eff9
sh-mirjalol-3930 <mirjalolshomurodov179@gmail.com>
2025-01-14 20:38:20 +0500

    8ta masala

--- script.js
+++ script.js
@@ -20,9 +20,59 @@ console.log(kb);
 
 
 let A = 1000;
-let B = 100
-let C = A / B 
+let B = 100;
+let C = A / B ;
 console.log(C);
+
+
+
+
+let ikkixonali_son = 55;
+
+if (ikkixonali_son >= 10 & ikkixonali_son <= 99) {
+  let onlik = Math.floor(ikkixonali_son / 10);
+  let birlik = ikkixonali_son % 10; 
+  console.log(onlik);
+  console.log(birlik);
+}else {
+  console.log();
+}
+
+
+
+
+let number = 66;
+
+if (number >= 10 & number <= 99) {
+  let tens = Math.floor(number / 10);
+  let units = number % 10;
+  let sum = tens + units;
+
+  console.log(sum);
+}else {
+  console.log();
+}
+
+
+
+
+
+
+
+let uchxonali = 123;
+
+if (uchxonali >= 100 & uchxonali <= 999) {
+  let hundreds = Math.floor(uchxonali / 100);
+  console.log(hundreds);
+}else {
+  console.log();
+}
+
+
+
+
+
+
 
 
 
